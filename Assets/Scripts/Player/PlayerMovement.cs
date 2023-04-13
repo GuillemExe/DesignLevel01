de7ef2d279bbe9215b23c48942acc14cea7f4190
GuillemExe <guillem.arrojo@gmail.com>
2023-04-13 22:57:29 +0200

    Fix

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
+using UnityEngine.EventSystems;
+using UnityEditor;
+using Unity.VisualScripting;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController m_CharacterController;
+    [SerializeField] private GameObject m_HitBox;
+
+    private Vector3 m_PlayerVelocity;
+    private bool m_GroundedPlayer;
+
+    public float speed = 25.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private float turner;
+    private float looker;
+    public float sensitivity = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        if (m_CharacterController == null && GetComponent<CharacterController>())
+            m_CharacterController = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EditorApplication.isPaused = true;
+        }
+
+        // is the controller on the ground?
+        if (m_CharacterController.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            //Jumping
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        turner = Input.GetAxis("Mouse X") * sensitivity;
+        looker = -Input.GetAxis("Mouse Y") * sensitivity;
+        if (turner != 0)
+        {
+            //Code for action on mouse moving right
+            transform.eulerAngles += new Vector3(0, turner, 0);
+        }
+        if (looker != 0)
+        {
+            //Code for action on mouse moving right
+            //transform.eulerAngles += new Vector3(looker, 0, 0);
+            m_HitBox.transform.rotation = Quaternion.Euler(0.0f, turner, gameObject.transform.rotation.z * -1.0f);
+        }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        m_CharacterController.Move(moveDirection * Time.deltaTime);
+    }
+}
