577a648ef094b33a28766c643f174678a8fbda17
GuillemExe <guillem.arrojo@gmail.com>
2023-04-20 14:56:23 +0200

    Adding system damage fall

--- Assets/Scripts/Player/CharacterControllerV1.cs
+++ Assets/Scripts/Player/CharacterControllerV1.cs
@@ -2,40 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.ProBuilder.AutoUnwrapSettings;
+
+public enum TypeFallDamage
+{
+    None = 0,
+    Low = 1,
+    Normal = 2,
+    Critical = 3,
+    InstaKill = 4
+}
 
 public class CharacterControllerV1 : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Velocidad de movimiento
-    public float jumpForce = 10f; // Fuerza del salto
-    public float jumpChargeRate = 0.5f; // Velocidad de carga del salto
-    public float jumpChargeLimit = 0.0f; // Límite de carga del salto
-    public float jumpChargeMax = 1f; // Máximo de carga del salto
-    public float lookSpeed = 5f; // Velocidad de rotación de la cámara
+    [Header("Basic movement")]
+    [SerializeField] private float moveSpeed = 5f; // Velocidad de movimiento
+    [SerializeField] private float jumpForce = 10f; // Fuerza del salto
+    [SerializeField] private float jumpChargeRate = 0.5f; // Velocidad de carga del salto
+    [SerializeField] private float jumpChargeLimit = 0.0f; // Límite de carga del salto
+    [SerializeField] private float jumpChargeMax = 1f; // Máximo de carga del salto
+    [SerializeField] private float lookSpeed = 5f; // Velocidad de rotación de la cámara
     [SerializeField] [Range(1f, 1.5f)] private float jumpOnUpMultiplier = 1.05f;
 
-    public float smoothness = 5f;
-    public float sensitivity = 2f;
-    private float _currentRotation;
+    [Header("Basic movement HUD")] 
+    [SerializeField] private Slider loadJumpSlider;
 
+    [Header("Camera movement")]
+    [SerializeField] private Transform camTransform; // Referencia a la transformación de la cámara
+    [SerializeField] private float smoothness = 5f;
+    [SerializeField] private float sensitivity = 2f;
+    private float currentRotation;
+
+    // Sistemas y controles de físicas, posible re-factorización
     private bool isJumping = false; // Indica si se está ejecutando el salto
     private float jumpCharge = 0.25f; // Cantidad de carga del salto
     private Rigidbody rb; // Referencia al Rigidbody
-    public Transform camTransform; // Referencia a la transformación de la cámara
+    
+    [Header("Player life")]
+    [SerializeField]
+    private float maxLife = 100.0f;
+    private float currentLife;
 
-    // DEBUG
+    [Header("Fall damage")]
+    [SerializeField] private float velocityYToHitLow = -15.0f;
+    [SerializeField] private float velocityYToHitNormal = -20.0f;
+    [SerializeField] private float velocityYToHitCritical = -25.0f;
+    [SerializeField] private float velocityYToHitInstaKill = -30.0f;
+    private TypeFallDamage typeFallDamage = TypeFallDamage.None;
+
+    [Header("Fall damage HUD")]
     [SerializeField] private TextMeshProUGUI fallVelocity;
+    private float saveVelocityYToHit;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        _currentRotation = transform.eulerAngles.y;
+        currentRotation = transform.eulerAngles.y;
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        loadJumpSlider.maxValue = jumpChargeMax;
+        loadJumpSlider.minValue = jumpCharge;
+
+        loadJumpSlider.gameObject.SetActive(false);
     }
 
     void Update()
     {
+        FallDamageCheck();
+
         fallVelocity.text = "Fall-Y: " + Mathf.Round(rb.velocity.y * 100f) / 10000f;
         
         // Rotar la cámara con el ratón
@@ -46,10 +83,10 @@ public class CharacterControllerV1 : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * sensitivity;
 
         // Actualizar la rotación actual del personaje
-        _currentRotation += mouseX;
+        currentRotation += mouseX;
 
         // Suavizar la rotación del personaje mediante una interpolación lineal
-        float angle = Mathf.LerpAngle(transform.eulerAngles.y, _currentRotation, Time.deltaTime * smoothness);
+        float angle = Mathf.LerpAngle(transform.eulerAngles.y, currentRotation, Time.deltaTime * smoothness);
         transform.eulerAngles = new Vector3(0f, angle, 0f);
 
         if (!isJumping)
@@ -58,6 +95,7 @@ public class CharacterControllerV1 : MonoBehaviour
             Vector3 moveDirection = camTransform.forward;
             moveDirection.y = 0f; // Asegurarse de que la dirección no tenga componente vertical
             moveDirection.Normalize(); // Normalizar la dirección para mantener la misma velocidad en todas las direcciones
+
 
             // Calcular la velocidad de movimiento y aplicarla al Rigidbody
             float horizontalInput = Input.GetAxis("Horizontal");
@@ -69,15 +107,28 @@ public class CharacterControllerV1 : MonoBehaviour
         // Saltar si se cumple la condición
         if (Input.GetKey(KeyCode.Space))
         {
+            // Mostramos el slider para enseñar la carga que lleva el jugador
+            loadJumpSlider.gameObject.SetActive(true);
+
+            // Congelamos la velocidad actual para hacer más preciso el salto, ojo que tienen cosas interesantes el quitarlo
+            rb.velocity = new Vector3(0, 0, 0);
+
             // Si se está manteniendo pulsada la tecla espacio
             if (jumpCharge < jumpChargeMax)
             {
                 jumpCharge += jumpChargeRate * Time.deltaTime; // Incrementar la carga del salto
+                loadJumpSlider.value = jumpCharge; // Actualizamos el HUD
             }
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
+            // Ocultamos el slider una vez ya comenzó el salto
+            loadJumpSlider.gameObject.SetActive(false);
+
+            // Congelamos la velocidad actual para hacer más preciso el salto, ojo que tienen cosas interesantes el quitarlo
+            rb.velocity = new Vector3(0, 0, 0);
+
             // Si se suelta la tecla espacio
             if (!isJumping && jumpCharge > jumpChargeLimit)
             {
@@ -90,11 +141,31 @@ public class CharacterControllerV1 : MonoBehaviour
         }
     }
 
+    void FallDamageCheck()
+    {
+        if (rb.velocity.y < velocityYToHitInstaKill)
+            typeFallDamage = TypeFallDamage.InstaKill;
+        else if (rb.velocity.y < velocityYToHitCritical)
+            typeFallDamage = TypeFallDamage.Critical;
+        else if (rb.velocity.y < velocityYToHitNormal)
+            typeFallDamage = TypeFallDamage.Normal;
+        else if (rb.velocity.y < velocityYToHitLow)
+            typeFallDamage = TypeFallDamage.Low;
+        else
+            typeFallDamage = TypeFallDamage.None;
+    }
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Ground")
         {
+            if (typeFallDamage != TypeFallDamage.None) // Comprobamos si tiene que recibir daño dada a su velocidad
+            {
+                Debug.Log("HIT, velocity y: " + saveVelocityYToHit);
+            }
+
             isJumping = false; // Marcar que se ha terminado el salto
+            typeFallDamage = TypeFallDamage.None; // Marcamos que no recibira más daño
         }
     }
 }
--- Assets/Scripts/Player/FallHitLife.cs
+++ Assets/Scripts/Player/FallHitLife.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class FallHitLife : MonoBehaviour
 {
+    private Rigidbody Rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Rb.velocity.y > -0.075f)
+        {
+            Debug.Log("HIT");
+        }
     }
 }
